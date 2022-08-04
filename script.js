c155b345aa7b74eec6bd733b821e111c436393fb
Elena <ukr.7905432@gmail.com>
2022-08-04 14:07:27 -0600

    load complementation
    первые задачи закомментированы

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/*let i = 1;
+while(i < 101) {
+    console.log(i);
+    i++;
+}*/
+
+/*let i = 100;
+while(i > 0) {
+    console.log(i);
+    i--;
+}*/
+
+/*for (let i = 1; i < 101; i++) {
+    if(i % 2 == 0) {
+console.log(i);
+    }
+    }*/
+
+/*const arr = [1, 2, 3, 4, 5];
+let sum = 0;
+for(let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+    console.log(sum);*/
+
+    const arr = [1, 2, 3, 4, 5];
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i]**2;
+    }
+        console.log(sum);
